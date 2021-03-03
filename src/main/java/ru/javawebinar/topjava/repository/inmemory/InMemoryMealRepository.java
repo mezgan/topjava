d6ee7eb7321ef4dacd716cd0f543d70c3aeb2f62
mezgang <mezgang@gmail.com>
2021-03-04 01:13:04 +0300

    patch 2.5

---
+++ src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepository.java
@@ -0,0 +1,28 @@
+package ru.javawebinar.topjava.repository.inmemory;
+
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.repository.MealRepository;
+
+import java.util.Collection;
+
+public class InMemoryMealRepository implements MealRepository {
+    @Override
+    public Meal save(Meal meal) {
+        return null;
+    }
+
+    @Override
+    public boolean delete(int id) {
+        return false;
+    }
+
+    @Override
+    public Meal get(int id) {
+        return null;
+    }
+
+    @Override
+    public Collection<Meal> getAll() {
+        return null;
+    }
+}
---
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -0,0 +1,5 @@
+package ru.javawebinar.topjava.web;
+
+public class MealServlet {
+
+}
